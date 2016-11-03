d1491f56378ad28b4cc24296efb5d6196f8c7c7f
AKabhishek <abhishekkaushik.star@gmail.com>
2016-11-03 14:45:24 +0000

    Aoscp-fy - Bring cheesecake for kenzo - Remove unknown type persist_block_device

--- aoscp.mk
+++ aoscp.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
 # Inherit from kenzo device
 $(call inherit-product, device/xiaomi/kenzo/device.mk)
@@ -12,12 +12,16 @@ $(call inherit-product, device/xiaomi/kenzo/device.mk)
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := xenonhd_kenzo
+PRODUCT_NAME := aoscp_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3 Pro
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot animation
+ TARGET_SCREEN_HEIGHT := 1920
+ TARGET_SCREEN_WIDTH := 1080
 
 # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
