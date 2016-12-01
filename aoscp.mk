80b6b44ee1b7b2a1f3b70addc7613b271544981a
AKabhishek <abhishekkaushik.star@gmail.com>
2016-12-01 14:32:17 +0000

    Broken bring up - Kenzo broked :p

--- aoscp.mk
+++ aoscp.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 # Inherit from kenzo device
 $(call inherit-product, device/xiaomi/kenzo/device.mk)
@@ -12,7 +12,7 @@ $(call inherit-product, device/xiaomi/kenzo/device.mk)
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := aoscp_kenzo
+PRODUCT_NAME := broken_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3 Pro
 TARGET_VENDOR := Xiaomi
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo broken_kenzo-userdebug
+
